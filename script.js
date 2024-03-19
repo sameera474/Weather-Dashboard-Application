8961381e7c373f8654591141f600e62398b9cb06
Sameera Chaturanga Wagaarachchige <sameera474@gmail.com>
2024-03-19 09:50:53 +0200

    Add celsius and fahrenheit

--- script.js
+++ script.js
@@ -1,16 +1,7 @@
 const apiKey = "92eee5c7adef3eb7ae8f9181b46f2b63";
 const weatherUrl = "https://api.openweathermap.org/data/2.5/weather";
 
-// const lat = Latitude;
-
-// const lon = Longitude;
-
-// const units = Unit;
-
-// const lang = language;
-
-//
-
+// Search Button
 document.getElementById("search-button").addEventListener("click", function () {
   // console.log("its click");
   let cityName = document.getElementById("search-input").value.trim();
@@ -22,19 +13,64 @@ document.getElementById("search-button").addEventListener("click", function () {
     alert("Please enter a city name");
   }
 });
-//function done
-async function fetchWeatherDetail(createdUrl, cityName) {
+
+//fetch function
+async function fetchWeatherDetail(createdUrl) {
   console.log("calling fetchWeatherDetail");
   try {
     const response = await fetch(createdUrl);
     const data = await response.json();
     console.log(data);
-    document.getElementById("city-name").innerText = data.name;
-    document.getElementById("temperature").innerText = data.main.temp;
-    document.getElementById("humidity").innerText = data.main.humidity;
-    // document.getElementById("wind-speed").innerText = windSpeed;
+    liveWeather(data);
   } catch (error) {
     console.error(error);
     alert("Failed to fetch weather data. Please try again.");
+  }
+}
+
+// Live Weather
+function liveWeather(data, temp) {
+  document.getElementById("country").innerText = data.sys.country;
+  document.getElementById("city-name").innerText = data.name;
+  document.getElementById("temperature").innerText = (
+    parseFloat(data.main.temp) - 273.15
+  ).toFixed(2);
+  document.getElementById("humidity").innerText = data.main.humidity;
+  document.getElementById("weather-icon").innerText = data.weather[0].icon;
+  document.getElementById("wind-speed").innerText = data.wind.speed;
+  let temperature = parseFloat(data.main.temp) - 273.15;
+  forToggleSwitch(temperature);
+}
+
+// Clear Button
+document.getElementById("clear-button").addEventListener("click", function () {
+  document.getElementById("country").innerText = "";
+  document.getElementById("city-name").innerText = "";
+  document.getElementById("temperature").innerText = "";
+  document.getElementById("humidity").innerText = "";
+  document.getElementById("weather-icon").innerText = "";
+  document.getElementById("wind-speed").innerText = "";
+});
+
+// celsius and fahrenheit
+
+function forToggleSwitch(temperature) {
+  console.log(temperature);
+  document
+    .getElementById("toggleSwitch")
+    .addEventListener("change", function () {
+      if (this.checked) {
+        celsiusToFahrenheit(temperature);
+        console.log(celsiusToFahrenheit(temperature));
+      } else {
+        fahrenheitToCelsius(temperature);
+        console.log(fahrenheitToCelsius(temperature));
+      }
+    });
+  function celsiusToFahrenheit(temperature) {
+    return ((temperature * 9) / 5 + 32).toFixed(2);
+  }
+  function fahrenheitToCelsius(temperature) {
+    return temperature.toFixed(2);
   }
 }
