067ae710241a95d035367ddcc6063bfbc9b6ec47
Sameera Chaturanga Wagaarachchige <sameera474@gmail.com>
2024-03-22 12:34:34 +0200

    add fore cast button

--- script.js
+++ script.js
@@ -1,5 +1,8 @@
 const apiKey = "92eee5c7adef3eb7ae8f9181b46f2b63";
-const weatherUrl = "https://api.openweathermap.org/data/2.5/weather";
+const weatherUrl = "https://api.openweathermap.org/data/2.5/";
+
+const latitude = 0;
+const longitude = 0;
 
 // Search Button
 document.getElementById("search-button").addEventListener("click", function () {
@@ -7,18 +10,43 @@ document.getElementById("search-button").addEventListener("click", function () {
   let cityName = document.getElementById("search-input").value.trim();
   if (cityName != "" && cityName != undefined) {
     console.log(cityName);
-    const createdUrl = `${weatherUrl}?q=${cityName}&appid=${apiKey}&units=metricdocument`;
-    fetchWeatherDetail(createdUrl, cityName);
+    const liveWheatherUrl = `${weatherUrl}weather?q=${cityName}&appid=${apiKey}&units=metricdocument`;
+    fetchWeatherDetail(liveWheatherUrl, cityName);
   } else {
     alert("Please enter a city name");
   }
+
+  const main_weather_container = document.querySelector(
+    ".main-weather-container"
+  );
+  // Check if the forecast button already exists to avoid duplication
+  let forecastButton = document.querySelector(".forecast-button");
+  if (!forecastButton) {
+    let forecat_button = document.createElement("button");
+    forecat_button.className = "forecast-button";
+    forecat_button.innerText = "Forecast";
+    main_weather_container.appendChild(forecat_button);
+
+    forecat_button.addEventListener("click", function () {
+      console.log("its click");
+      let cityName = document.getElementById("search-input").value.trim();
+      if (cityName != "" && cityName != undefined) {
+        console.log(cityName);
+        const forecastWheatherUrl = `${weatherUrl}forecast/daily?lat=${latitude}&lon=${longitude}&cnt=${14}&appid=${apiKey}`;
+        console.log("latitude:", latitude);
+        fetchForecastWeatherDetail(forecastWheatherUrl, cityName);
+      } else {
+        alert("Please enter a city name");
+      }
+    });
+  }
 });
 
-//fetch function
-async function fetchWeatherDetail(createdUrl) {
+//fetch function live weather
+async function fetchWeatherDetail(liveWheatherUrl) {
   console.log("calling fetchWeatherDetail");
   try {
-    const response = await fetch(createdUrl);
+    const response = await fetch(liveWheatherUrl);
     const data = await response.json();
     console.log(data);
     liveWeather(data);
@@ -28,22 +56,64 @@ async function fetchWeatherDetail(createdUrl) {
   }
 }
 
+// fetch function forecast weather
+async function fetchForecastWeatherDetail(forecastWheatherUrl) {
+  console.log("calling fetchWeatherDetail");
+  try {
+    const response = await fetch(forecastWheatherUrl);
+    const data = await response.json();
+    console.log(data);
+    forecastWeather(data);
+  } catch (error) {
+    console.error(error);
+    alert("Failed to fetch weather data. Please try again.");
+  }
+}
+
 // Live Weather
 function liveWeather(data) {
-  document.getElementById("country").innerText = `Country ${data.sys.country}`;
-  document.getElementById("city-name").innerText = `City ${data.name}`;
-  document.getElementById("temperature").innerText = `Temperature ${(
-    parseFloat(data.main.temp) - 273.15
-  ).toFixed(2)} °C`;
+  document.getElementById("country").innerText = `${data.sys.country}`;
+  document.getElementById("city-name").innerText = `${data.name}`;
   document.getElementById(
     "humidity"
   ).innerText = `Humidity ${data.main.humidity}`;
-  document.getElementById("weather-icon").innerText = data.weather[0].icon;
+  const iconCode = data.weather[0].icon;
+  const iconUrl = ` https://openweathermap.org/img/wn/${iconCode}@2x.png`;
+  document.getElementById("weather-icon").src = iconUrl;
   document.getElementById(
     "wind-speed"
   ).innerText = `Wind Speed ${data.wind.speed}`;
-  let temperature = parseFloat(data.main.temp) - 273.15;
+  const temperature = (parseFloat(data.main.temp) - 273.15).toFixed(1);
+  document.getElementById("temperature").innerText = `${temperature} °C`;
   forToggleSwitch(temperature);
+  const forecastWheatherUrl = `${weatherUrl}forecast/daily?lat=${latitude}&lon=${longitude}&cnt=${14}&appid=${apiKey}`;
+  latitude = data.coord.lat;
+  longitude = data.coord.lon;
+  // return latitude, longitude;
+  console.log("latitude:", latitude);
+}
+
+function forecastWeather(data) {
+  console.log("forecastWeather");
+  // document.getElementById("date").innerText = `${data.sys.country}`;
+  // document.getElementById("city-name").innerText = `${data.name}`;
+  // document.getElementById("feels-like-temperature").innerText = `${data.name}`;
+  // document.getElementById(
+  //   "humidity-date"
+  // ).innerText = `Humidity ${data.main.humidity}`;
+  // const iconCode = data.weather[0].icon;
+  // const iconUrl = ` https://openweathermap.org/img/wn/${iconCode}@2x.png`;
+  // document.getElementById("weather-icon-date").src = iconUrl;
+  // document.getElementById(
+  //   "wind-speed-date"
+  // ).innerText = `Wind Speed ${data.wind.speed}`;
+  // const temperature = (parseFloat(data.main.temp) - 273.15).toFixed(1);
+  // document.getElementById("temperature-date").innerText = `${temperature} °C`;
+  // forToggleSwitch(temperature);
+  // const latitude = data.coord.lat;
+  // const longitude = data.coord.lon;
+  // const liveWheatherUrl = `${weatherUrl}forecast/daily?lat=${latitude}&lon=${longitude}&cnt=${14}&appid=${apiKey}`;
+  // const forecastUrl = liveWheatherUrl;
 }
 
 // celsius and fahrenheit
@@ -56,20 +126,20 @@ function forToggleSwitch(temperature) {
       if (this.checked) {
         document.getElementById(
           "temperature"
-        ).innerText = `Temperature , ${celsiusToFahrenheit(temperature)} °F`;
+        ).innerText = `${celsiusToFahrenheit(temperature)} °F`;
         console.log(celsiusToFahrenheit(temperature));
       } else {
         document.getElementById(
           "temperature"
-        ).innerText = `Temperature , ${fahrenheitToCelsius(temperature)} °C`;
+        ).innerText = `${fahrenheitToCelsius(temperature)} °C`;
         console.log(fahrenheitToCelsius(temperature));
       }
     });
   function celsiusToFahrenheit(temperature) {
-    return ((temperature * 9) / 5 + 32).toFixed(2);
+    return ((temperature * 9) / 5 + 32).toFixed(1);
   }
   function fahrenheitToCelsius(temperature) {
-    return temperature.toFixed(2);
+    return temperature;
   }
 }
 
@@ -82,4 +152,6 @@ document.getElementById("clear-button").addEventListener("click", function () {
   document.getElementById("humidity").innerText = "";
   document.getElementById("weather-icon").innerText = "";
   document.getElementById("wind-speed").innerText = "";
+  document.getElementById("weather-icon").src = "";
+  document.getElementById("toggleSwitch").checked = false;
 });
